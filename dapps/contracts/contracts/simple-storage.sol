6b0064b8267763bc001fda35161a33c5adfad1df
MaulanaSandyy <maulanasandy.official@gmail.com>
2026-01-16 15:50:03 +0700

    perbaikan sedikit tapi sama seperti submmision dan update code ini disaat saya sudah submission day 2

--- dapps/contracts/contracts/simple-storage.sol
+++ dapps/contracts/contracts/simple-storage.sol
@@ -2,29 +2,51 @@
 pragma solidity ^0.8.20;
 
 contract SimpleStorage {
+    // menyimpan nilai dalam bentuk uint256
     uint256 private storedValue;
+    //tracking saat update nilai
+    event ValueUpdated(uint256 newValue);
+
+    // menyimpan nilai ke blockchain (Write)
+    function setValue(uint256 _value) public {
+        storedValue = _value;
+        emit ValueUpdated(_value);
+    }
+    //baca nilai dari blockchain (Read) terakhir kali di update
+    function getValue() public view returns (uint256) {
+        return storedValue;
+    }
+
+    // Tambahan Buat Homework + Optional Homework 3.4
+
+     // owner dan access control
     address public owner;
 
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
-    event ValueUpdated(uint256 newValue);
 
+    // state tambahan (misal: message)
+    string public message;
+
+    // constructor untuk set owner dan emit OwnerSet
     constructor() {
-        address oldOwner = owner;
         owner = msg.sender;
-        emit OwnerSet(oldOwner, owner);
+        emit OwnerSet(address(0), owner);
     }
 
+    // modifier onlyOwner
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
         _;
     }
 
-    function setValue(uint256 _value) public onlyOwner {
+    // override setValue untuk akses kontrol
+    function setValueWithOwner(uint256 _value) public onlyOwner {
         storedValue = _value;
         emit ValueUpdated(_value);
     }
 
-    function getValue() public view returns (uint256) {
-        return storedValue;
+    // fungsi tambahan untuk update message
+    function setMessage(string calldata _message) public onlyOwner {
+        message = _message;
     }
 }
